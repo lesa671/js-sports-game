c0d053c1a3b6ac81aeb6e354c0c8ff083d6b6bce
Lesa Ducksworth <lducks67@gmail.com>
2020-05-04 04:14:20 -0500

    added more styling to css file, added alert function to js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,4 @@
-const reset_button = document.querySelector('#reset_button')
-const num_resets = document.querySelector('#num_resets')
-reset_button.addEventListener('click', function () {
-    console.log('reset_button was clicked')
-    teamone_numshots.innerText = 0 
-    teamone_shot_count = 0
-    teamone_numgoals.innerText = 0 
-    teamtwo_numshots.innerText = 0
-    teamtwo_shot_count = 0 
-    teamtwo_numgoals.innerText = 0
-     
-    num_resets.innerHTML = parseInt(num_resets.innerText) + 1
-})
+
 
 let teamone_shot_count = 0
 const teamone_numshots = document.querySelector('#teamone_numshots')
@@ -21,11 +9,12 @@ teamone_shoot_button.addEventListener('click', function () {
     teamone_shot_count = teamone_shot_count + 1
     teamone_numshots.innerText =  teamone_shot_count
     let teamone_goal_result = parseInt(teamone_numgoals.innerText) + Math.floor(Math.random() * 2)
-    teamone_numgoals.innerText = teamone_goal_result   
+    teamone_numgoals.innerText = teamone_goal_result
 })
 
 let teamtwo_shot_count = 0
 const teamtwo_numshots = document.querySelector('#teamtwo_numshots')
+const teamtwo_numgoals = document.querySelector('#teamtwo_numgoals')
 const teamtwo_shoot_button = document.querySelector('#teamtwo_shoot_button')
 teamtwo_shoot_button.addEventListener('click', function () {
     console.log('teamtwo_shoot_button clicked')
@@ -35,4 +24,25 @@ teamtwo_shoot_button.addEventListener('click', function () {
     teamtwo_numgoals.innerText = teamtwo_goal_result  
 })
 
+const reset_button = document.querySelector('#reset_button')
+const num_resets = document.querySelector('#num_resets')
+reset_button.addEventListener('click', function () {
+    console.log('reset_button was clicked')
 
+    if (teamone_numgoals.innerText > teamtwo_numgoals.innerText) {
+        alert('Team 1 is the winner!')
+    } else if (teamtwo_numgoals.innerText > teamone_numgoals.innerText) {
+        alert('Team 2 is the winner!')
+    } else {
+        alert('It\'s a tie!')
+    } 
+    
+    teamone_numshots.innerText = 0 
+    teamone_shot_count = 0
+    teamone_numgoals.innerText = 0 
+    teamtwo_numshots.innerText = 0
+    teamtwo_shot_count = 0 
+    teamtwo_numgoals.innerText = 0
+     
+    num_resets.innerHTML = parseInt(num_resets.innerText) + 1
+})
